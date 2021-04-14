ec416c94d1ccafc350505363896d1047e4f3ef3b
Your Name <you@example.com>
2021-04-14 15:25:29 +0600

    Added

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -5,7 +5,7 @@ import PostList from "./PostList/PostList"
 const Posts = ({ match }) => {
     return ( 
         <div className="posts">
-{match.isExact}
+          {match.isExact}
         <PostList/>
         <Route path="/posts/:id" component={PostFull}/>
         </div>
